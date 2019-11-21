58174488f58d4b1d9aef79daf8b837e1e45c63fa
Thibault Boursier <boursier.thibault@gmail.com>
2019-11-21 01:03:22 +0100

    feat(continent): makes som try with GQL fragment

--- src/queries/countries.tsx
+++ src/queries/countries.tsx
@@ -1,14 +1,21 @@
 import gql from "graphql-tag.macro";
 
+const continentFragment = gql`
+  fragment continentFragment on Continent {
+    name
+  }
+`;
+
 export const GET_COUNTRIES = gql`
   {
     countries {
       code
       continent {
-        name
+        ...continentFragment
       }
       emoji
       name
     }
   }
+  ${continentFragment}
 `;
